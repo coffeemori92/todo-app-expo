46a89b0fe7ab47ba1303195215a341b7dfd28c3a
coffeemori <coffeemori92@naver.com>
2022-05-24 18:20:16 +0900

    fin

--- App.js
+++ App.js
@@ -1,11 +1,92 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView, Alert } from 'react-native';
+import { Fontisto } from '@expo/vector-icons';
+import { theme } from './colors';
+
+const STORAGE_KEY = '@toDos';
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState('');
+  const [toDos, setToDos] = useState({});
+
+  const travel = useCallback(() => setWorking(false), [working]);
+  const work = useCallback(() => setWorking(true), [working]);
+  const onChangeText = useCallback(payload => setText(payload), [text]);
+  const addTodo = useCallback(async () => {
+    if (text) {
+      const newToDos = { ...toDos, [Date.now()]: { text, working } };
+      setToDos(newToDos);
+      await saveToDos(newToDos);
+      setText('');
+    }
+  }, [text]);
+  const saveToDos = useCallback(async toSave => {
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave));
+  }, []);
+  const loadToDos = useCallback(async () => {
+    const s = await AsyncStorage.getItem(STORAGE_KEY);
+    setToDos(JSON.parse(s));
+  }, []);
+  const deleteToDo = useCallback(
+    key => {
+      Alert.alert('Delete To Do', 'Are you sure?', [
+        { text: 'Cancel' },
+        {
+          text: "I'm sure",
+          onPress: async () => {
+            const newToDos = { ...toDos };
+            delete newToDos[key];
+            setToDos(newToDos);
+            await saveToDos(newToDos);
+          },
+        },
+      ]);
+    },
+    [toDos]
+  );
+
+  useEffect(() => {
+    loadToDos();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+          <Text style={{ ...styles.btnText, color: working ? 'white' : theme.gray }}>Work</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text style={{ ...styles.btnText, color: !working ? 'white' : theme.gray }}>Travel</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TextInput
+          onSubmitEditing={addTodo}
+          onChangeText={onChangeText}
+          returnKeyType="done"
+          value={text}
+          placeholder={working ? 'Add a To Do' : 'Where do you want to go?'}
+          style={styles.input}
+        />
+        <ScrollView>
+          {Object.keys(toDos).map(key =>
+            toDos[key].working === working ? (
+              <View style={styles.toDo} key={key}>
+                <Text style={styles.toDoTxt}>{toDos[key].text}</Text>
+                <TouchableOpacity onPress={() => deleteToDo(key)}>
+                  <Text>
+                    <Fontisto name="trash" size={18} color={theme.gray} />
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            ) : null
+          )}
+        </ScrollView>
+      </View>
     </View>
   );
 }
@@ -13,8 +94,40 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 50,
+  },
+  btnText: {
+    fontSize: 38,
+    fontWeight: '600',
+    color: 'white',
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginVertical: 20,
+    fontSize: 18,
+  },
+  toDo: {
+    backgroundColor: theme.toDoBg,
+    marginBottom: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 15,
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+  },
+  toDoTxt: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '500',
   },
 });
